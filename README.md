d585c8a2dcf8e0240a4fd672e16a4058ba70df3b
CW987 <inward-panting0f@icloud.com>
2024-12-15 16:01:36 +0000

    JS to generate images and make them interactive

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 References:
 Coding all in one for dummies - Minnick, C. et al.
+
+
+One of the odd stumbling blocks I have noticed this week is not trusting in my own knowledge or that I haven't quite linked the names to the ideas. Carefully following the walking skeleton lesson, I didn't look back enough at what I had already done earlier in the week, my own code examples were already there along with the notes as to what methods I preferred.
+
+Noticed my pictures were going up in file size once I had them together, so was good to see the lag in loading the larger images when testing the website. 
+
+I hit a wall after getting the one image to display. I couldn't mentally piece together what I needed next in order to build a thumbnail bar. So I fed my code, the code of the example website,and another students, so it could tell me what was missing and where to go next. Initial set up of the website was easier this week so I assume this is another thing that will just come with practice.
--- app.js
+++ app.js
@@ -1,34 +1,66 @@
-console.log("test my update")
+console.log("test my update");
 
 const images = [
-    { src: "./SalvationArmy.jpg", alt: "Abandoned Salvation Army building in Sheffield, England with plants growing out of the facade", id:"salvationArmy" },
-    { src: "./Palace.jpg", alt: "Abandoned Bishop's Palace, St. Davids, Wales", id:"Palace"
-    },
-    { src: "./Seminary.jpg", alt: "St. Peter's Seminary - An abandoned Brutalist building in the Scottish countryside, near Cardross", id:"seminary" },
-    { src: "./Castle.jpg", alt: "Abandoned Castle, Girvan Scotland. Interior view looking up through the ruined keep, decorational timbers still visible on higher floors, no floors left", id:"Castle" },
-    { src: "./Bunker.jpg", alt: "Abandoned World War 2 pillbox bunker that has slipped down onto the beach. Redcar England", id:"bunker" },
-    // { src: "url", alt: "description" }
-]
-const thumbnailCont = document.getElementById("thumbnailCont")
-const mainImgCont = document.getElementById("mainImgCont")
+  {
+    src: "./SalvationArmy.jpg",
+    alt: "Abandoned Salvation Army building in Sheffield, England with plants growing out of the facade",
+    id: "salvationArmy",
+  },
+  {
+    src: "./Palace.jpg",
+    alt: "Abandoned Bishop's Palace, St. Davids, Wales",
+    id: "Palace",
+  },
+  {
+    src: "./Seminary.jpg",
+    alt: "St. Peter's Seminary - An abandoned Brutalist building in the Scottish countryside, near Cardross",
+    id: "seminary",
+  },
+  {
+    src: "./Castle.jpg",
+    alt: "Abandoned Castle, Girvan Scotland. Interior view looking up through the ruined keep, decorational timbers still visible on higher floors, no structural timer left",
+    id: "Castle",
+  },
+  {
+    src: "./Bunker.jpg",
+    alt: "Abandoned World War 2 pillbox bunker that has slipped down onto the beach. Redcar England",
+    id: "bunker",
+  },
+  // { src: "url", alt: "description" }
+];
 
-// let thumbnailCont = document.getElementById('thumbnailCont')
-let displayedImg = document.createElement("img")
-displayedImg.src = images [0].src
-displayedImg.alt = images [0].alt
-displayedImg.className = "displayedImg"
-mainImgCont.appendChild(displayedImg)
+// This gets the containers for the main image and thumbnail.
+const thumbnailCont = document.getElementById("thumbnailCont");
+const mainImgCont = document.getElementById("mainImgCont");
 
+// This creates and displays the first image in the array as the main image
+let displayedImg = document.createElement("img");
+displayedImg.src = images[0].src;
+// the number after images is which image from the array will be displayed first when the website loads
+displayedImg.alt = images[0].alt;
+displayedImg.className = "displayedImg";
+mainImgCont.appendChild(displayedImg);
 
-// for (let image of images) {
-//     console.log(image)
-//     // image = src
-// }
+// This updates the main image when a thumbnail is clicked
+function updateMainImage(index) {
+  console.log("Main image updated to:", images[index].alt);
+  displayedImg.src = images[index].src;
+  displayedImg.alt = images[index].alt;
+}
 
-// function createThumbnails(images){
+// This creates the thumbnails
+function createThumbnails(imageIndex) {
+  console.log("createThumbnails used");
+  const thumbnailCont = document.querySelector("#thumbnailCont");
+  imageIndex.forEach((image, index) => {
+    const thumbnail = document.createElement("img");
+    thumbnail.src = image.src;
+    thumbnail.alt = image.alt;
+    thumbnail.className = "thumbnail";
+    thumbnail.addEventListener("click", () => updateMainImage(index));
+    thumbnailCont.appendChild(thumbnail);
+  });
 
-// }
+}
 
-// function createMainImg(mainImage){
-//     mainImgContainer.innerHTML = null
-// }
+createThumbnails(images)
